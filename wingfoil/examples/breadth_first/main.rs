93966cdbfc1c1da5a4dd185cda98894ebbf384af
Jake Mitchell <11303841+0-jake-0@users.noreply.github.com>
2025-11-30 20:14:22 +0000

    clippy

--- wingfoil/examples/breadth_first/main.rs
+++ wingfoil/examples/breadth_first/main.rs
@@ -42,7 +42,9 @@ fn main() {
         source = add(&source, &source);
     }
     let cycles = source.count();
-    cycles.run(RunMode::HistoricalFrom(NanoTime::ZERO), RunFor::Forever).unwrap();
+    cycles
+        .run(RunMode::HistoricalFrom(NanoTime::ZERO), RunFor::Forever)
+        .unwrap();
     println!("cycles {:?}", cycles.peek_value());
     println!("value {:?}", source.peek_value());
 }
