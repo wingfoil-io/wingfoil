1dbae8c9a569ddef434973abc71ca5888c988f91
Jake Mitchell <11303841+0-jake-0@users.noreply.github.com>
2025-12-21 18:21:19 +0000

    version=>1.0.0
    + fix action

--- wingfoil-python/docs/conf.py
+++ wingfoil-python/docs/conf.py
@@ -8,7 +8,7 @@ sys.modules['wingfoil.wingfoil'] = wingfoil # some workaround for sphinx nonsens
 
 copyright = '2025, Jake Mitchell'
 author = 'Jake Mitchell'
-release = '0.1.15'
+release = '1.0.0'
 
 # -- General configuration ---------------------------------------------------
 extensions = [
