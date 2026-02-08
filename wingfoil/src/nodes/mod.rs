f92103058789865b0e0cb0ca2464595b2a928b3e
Jake Mitchell <11303841+0-jake-0@users.noreply.github.com>
2026-02-08 19:07:00 +0000

    Reinstate remaining files from main and update consume_async to return Result

--- wingfoil/src/nodes/mod.rs
+++ wingfoil/src/nodes/mod.rs
@@ -28,6 +28,7 @@ mod print;
 mod producer;
 mod sample;
 mod tick;
+mod try_bimap;
 mod try_map;
 mod window;
 
@@ -57,6 +58,7 @@ use print::*;
 use producer::*;
 use sample::*;
 use tick::*;
+use try_bimap::*;
 use try_map::*;
 use window::WindowStream;
 
@@ -82,16 +84,33 @@ where
     T: Element + Add<Output = T>,
 {
     let f = |a: T, b: T| (a + b) as T;
-    BiMapStream::new(upstream1.clone(), upstream2.clone(), Box::new(f)).into_stream()
+    BiMapStream::new(
+        Dep::Active(upstream1.clone()),
+        Dep::Active(upstream2.clone()),
+        Box::new(f),
+    )
+    .into_stream()
 }
 
-/// Maps two [Stream]s into one using the supplied function.  Ticks when either of it's sources ticks.
+/// Maps two [Stream]s into one using the supplied function.
+/// Use [Dep::Active] and [Dep::Passive] to control which upstreams trigger execution.
 pub fn bimap<IN1: Element, IN2: Element, OUT: Element>(
-    upstream1: Rc<dyn Stream<IN1>>,
-    upstream2: Rc<dyn Stream<IN2>>,
+    upstream1: Dep<IN1>,
+    upstream2: Dep<IN2>,
     func: impl Fn(IN1, IN2) -> OUT + 'static,
 ) -> Rc<dyn Stream<OUT>> {
     BiMapStream::new(upstream1, upstream2, Box::new(func)).into_stream()
+}
+
+/// Maps two [Stream]s into one using a fallible closure.
+/// Use [Dep::Active] and [Dep::Passive] to control which upstreams trigger execution.
+/// Errors propagate to graph execution.
+pub fn try_bimap<IN1: Element, IN2: Element, OUT: Element>(
+    upstream1: Dep<IN1>,
+    upstream2: Dep<IN2>,
+    func: impl Fn(IN1, IN2) -> anyhow::Result<OUT> + 'static,
+) -> Rc<dyn Stream<OUT>> {
+    TryBiMapStream::new(upstream1, upstream2, Box::new(func)).into_stream()
 }
 
 /// Returns a stream that merges it's sources into one.  Ticks when either of it's sources ticks.
@@ -251,9 +270,18 @@ pub trait StreamOperators<T: Element> {
     where
         T: Element + Send,
         FUT: Future<Output = anyhow::Result<()>> + Send + 'static;
-    fn finally<F: FnOnce(T, &GraphState) + 'static>(self: &Rc<Self>, func: F) -> Rc<dyn Node>;
+    fn finally<F: FnOnce(T, &GraphState) -> anyhow::Result<()> + 'static>(
+        self: &Rc<Self>,
+        func: F,
+    ) -> Rc<dyn Node>;
     /// executes supplied closure on each tick
     fn for_each(self: &Rc<Self>, func: impl Fn(T, NanoTime) + 'static) -> Rc<dyn Node>;
+    /// executes supplied fallible closure on each tick.
+    /// Errors propagate to graph execution.
+    fn try_for_each(
+        self: &Rc<Self>,
+        func: impl Fn(T, NanoTime) -> anyhow::Result<()> + 'static,
+    ) -> Rc<dyn Node>;
     // reduce/fold source by applying function
     fn fold<OUT: Element>(
         self: &Rc<Self>,
@@ -323,12 +351,6 @@ pub trait StreamOperators<T: Element> {
     /// Map's it's source into a new Stream using the supplied closure.
     fn map<OUT: Element>(self: &Rc<Self>, func: impl Fn(T) -> OUT + 'static)
     -> Rc<dyn Stream<OUT>>;
-    /// Combines map and filter: transforms and optionally filters values.
-    /// The closure returns (OUT, bool) where bool indicates whether to emit.
-    fn filter_map<OUT: Element>(
-        self: &Rc<Self>,
-        func: impl Fn(T) -> (OUT, bool) + 'static,
-    ) -> Rc<dyn Stream<OUT>>;
     /// Map's source into a new Stream using a fallible closure.
     /// Errors propagate to graph execution.
     fn try_map<OUT: Element>(
@@ -383,8 +405,8 @@ where
 
     fn collect(self: &Rc<Self>) -> Rc<dyn Stream<Vec<ValueAt<T>>>> {
         bimap(
-            self.clone(),
-            self.clone().as_node().ticked_at(),
+            Dep::Active(self.clone()),
+            Dep::Active(self.clone().as_node().ticked_at()),
             ValueAt::new,
         )
         .fold(|acc: &mut Vec<ValueAt<T>>, value| {
@@ -466,6 +488,13 @@ where
         ConsumerNode::new(self.clone(), Box::new(func)).into_node()
     }
 
+    fn try_for_each(
+        self: &Rc<Self>,
+        func: impl Fn(T, NanoTime) -> anyhow::Result<()> + 'static,
+    ) -> Rc<dyn Node> {
+        TryConsumerNode::new(self.clone(), Box::new(func)).into_node()
+    }
+
     fn delay(self: &Rc<Self>, duration: Duration) -> Rc<dyn Stream<T>>
     where
         T: Hash + Eq,
@@ -499,7 +528,10 @@ where
         FilterStream::new(self.clone(), condition).into_stream()
     }
 
-    fn finally<F: FnOnce(T, &GraphState) + 'static>(self: &Rc<Self>, func: F) -> Rc<dyn Node> {
+    fn finally<F: FnOnce(T, &GraphState) -> anyhow::Result<()> + 'static>(
+        self: &Rc<Self>,
+        func: F,
+    ) -> Rc<dyn Node> {
         FinallyNode::new(self.clone(), Some(func)).into_node()
     }
 
@@ -522,8 +554,8 @@ where
                 value
             };
             bimap(
-                self.clone(),
-                self.clone().as_node().ticked_at_elapsed(),
+                Dep::Active(self.clone()),
+                Dep::Active(self.clone().as_node().ticked_at_elapsed()),
                 func,
             )
         } else {
@@ -536,13 +568,6 @@ where
         func: impl Fn(T) -> OUT + 'static,
     ) -> Rc<dyn Stream<OUT>> {
         MapStream::new(self.clone(), Box::new(func)).into_stream()
-    }
-
-    fn filter_map<OUT: Element>(
-        self: &Rc<Self>,
-        func: impl Fn(T) -> (OUT, bool) + 'static,
-    ) -> Rc<dyn Stream<OUT>> {
-        MapFilterStream::new(self.clone(), Box::new(func)).into_stream()
     }
 
     fn try_map<OUT: Element>(
