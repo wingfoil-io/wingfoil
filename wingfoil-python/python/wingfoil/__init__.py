fa2dcbebecf96b07f5c12a751e36a2e5eddafcb5
ZohaibHassan16 <zohaib179949@gmail.com>
2026-02-18 02:37:48 +0500

    chore: export pandas helpers in __init__

--- wingfoil-python/python/wingfoil/__init__.py
+++ wingfoil-python/python/wingfoil/__init__.py
@@ -3,9 +3,11 @@ from ._wingfoil import *
 from .stream import *
 
 from .pandas_helpers import to_dataframe, stream_to_dataframe
+
 __doc__ = _ext.__doc__
 __version__ = getattr(_ext, "__version__", None)
-__all__ = _ext.__all__ + ["to_dataframe", "stream_to_dataframe"]
+
+__all__ = list(_ext.__all__) + ["to_dataframe", "stream_to_dataframe"]
 
 # User-friendly aliases for KDB+ functions
 kdb_read = _ext.py_kdb_read
