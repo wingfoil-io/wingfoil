b4883f1bafee2c34bacf875b53737351c396b8a5
Pratham Mohan <pratham.parker14@gmail.com>
2026-03-12 21:41:06 +0530

    added pytest for inspect operator

--- wingfoil-python/tests/test_streams.py
+++ wingfoil-python/tests/test_streams.py
@@ -1,4 +1,6 @@
 import unittest
+
+#from httpcore import stream
 from wingfoil import constant, ticker
 
 class TestStreams(unittest.TestCase):
@@ -32,6 +34,18 @@ class TestStreams(unittest.TestCase):
         )        
         stream.run(realtime=False, cycles = 5)
         self.assertEqual(stream.peek_value(), [0, 1, 2])
+
+    def test_inspect(self):
+        inspected_values = []
+        stream = (
+        ticker(0.1)
+            .count()
+            .inspect(lambda x: inspected_values.append(x))
+            .collect()
+        )
+        stream.run(realtime=False, cycles=3)
+        self.assertEqual(inspected_values, [1, 2, 3])        # lambda was called
+        self.assertEqual(stream.peek_value(), [1, 2, 3])     # values passed through unchanged
 
 if __name__ == '__main__':
     unittest.main()
