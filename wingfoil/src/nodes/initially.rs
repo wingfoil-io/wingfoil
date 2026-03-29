8c4e7512b53d6250881b3d359cd3bc32b20808f7
Jake Mitchell <11303841+0-jake-0@users.noreply.github.com>
2026-03-29 23:46:43 +0100

    feat: add initially node mirroring finally pattern (#170)
    Calls a closure once in start() before the first cycle, taking &GraphState.

---
+++ wingfoil/src/nodes/initially.rs
@@ -0,0 +1,27 @@
+use crate::types::*;
+use derive_new::new;
+use std::rc::Rc;
+
+#[derive(new)]
+pub struct InitiallyNode<T: Element, F: FnOnce(&GraphState) -> anyhow::Result<()>> {
+    source: Rc<dyn Stream<T>>,
+    initially: Option<F>,
+    #[new(default)]
+    value: T,
+}
+
+impl<T: Element, F: FnOnce(&GraphState) -> anyhow::Result<()>> MutableNode for InitiallyNode<T, F> {
+    fn start(&mut self, state: &mut GraphState) -> anyhow::Result<()> {
+        if let Some(f) = self.initially.take() {
+            f(state)?;
+        }
+        Ok(())
+    }
+    fn cycle(&mut self, _state: &mut GraphState) -> anyhow::Result<bool> {
+        self.value = self.source.peek_value();
+        Ok(true)
+    }
+    fn upstreams(&self) -> UpStreams {
+        UpStreams::new(vec![self.source.clone().as_node()], vec![])
+    }
+}
