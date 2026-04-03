a264ff77a76df6b0aa3911c9d42e37ac6813c7d7
Jake Mitchell <11303841+0-jake-0@users.noreply.github.com>
2026-04-03 14:04:20 +0100

    feat(grafana): use rate(wingfoil_ticks_total[1s]) with 1s auto-refresh in explore URL

--- wingfoil/examples/grafana/run.sh
+++ wingfoil/examples/grafana/run.sh
@@ -28,7 +28,7 @@ fi
 
 trap 'echo ""; echo "==> Stopping Docker stack..."; $COMPOSE down' EXIT
 
-EXPLORE_URL='http://localhost:3000/explore?orgId=1&refresh=5s&left=%7B%22datasource%22%3A%22Prometheus%22%2C%22queries%22%3A%5B%7B%22refId%22%3A%22A%22%2C%22expr%22%3A%22wingfoil_ticks_total%22%7D%5D%2C%22range%22%3A%7B%22from%22%3A%22now-1m%22%2C%22to%22%3A%22now%22%7D%7D'
+EXPLORE_URL='http://localhost:3000/explore?orgId=1&refresh=1s&left=%7B%22datasource%22%3A%22Prometheus%22%2C%22queries%22%3A%5B%7B%22refId%22%3A%22A%22%2C%22expr%22%3A%22rate%28wingfoil_ticks_total%5B1s%5D%29%22%7D%5D%2C%22range%22%3A%7B%22from%22%3A%22now-1m%22%2C%22to%22%3A%22now%22%7D%7D'
 
 link() { printf '\e]8;;%s\e\\%s\e]8;;\e\\\n' "$1" "$2"; }
 
